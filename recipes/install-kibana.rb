dff4813944aac6e64fa1194beac955951b5f4df7
Aymen Chetoui <aymen.chetoui@gmail.com>
2015-08-28 00:20:04 +0100

    ELK Stack

---
+++ recipes/install-kibana.rb
@@ -0,0 +1,44 @@
+# Download Kibana 4 to home directory 
+
+execute "Download Kibana" do
+	command "cd ~; wget https://download.elasticsearch.org/kibana/kibana/kibana-4.0.1-linux-x64.tar.gz"
+end
+
+# Extract Kibana archive
+
+execute "Extract Kibana" do
+	command "tar xvf kibana-*.tar.gz"
+end
+
+# Modify Kibana yaml config file
+
+template "~/kibana-4*/config/kibana.yml" do
+	source "kibana.yml.erb"
+	mode "0644"
+end
+
+# Create a Kibana directory
+
+#execute "Add Kibana directory" do
+#	command "mkdir -p /opt/kibana"
+#end
+
+# Copying the Kibana files into Kibana directory:
+
+#execute "Copy Kibana files" do
+#	command "sudo cp -R ~/kibana-4*/* /opt/kibana/"
+#end
+
+# Create systemd for Kibana
+
+#cookbook_file "/etc/systemd/system/kibana4.service" do
+#	source "kibana4.service"
+#	mode "0644"
+#end
+
+# Start Kibana
+
+execute "Start Kibana" do
+	command "nohup ./kibana-4.0.1-linux-x64/bin/kibana &"
+end
+
---
+++ recipes/install-nginx.rb
@@ -0,0 +1,22 @@
+# Add the EPEL repository to yum
+
+execute "Add the EPEL repository to yum" do
+	command "yum -y install epel-release"
+end
+
+# Install Nginx and httpd-tools:
+
+execute "Install Nginx and httpd-tools" do
+	command "yum -y install nginx httpd-tools"
+end
+
+# Modify Kibana yaml config file
+
+cookbook_file "/etc/nginx/nginx.conf" do
+	source "nginx.conf"
+	mode "0644"
+end
+
+
+
+
