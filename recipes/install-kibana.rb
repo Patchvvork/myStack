306b5128cec0776cf5ecf6c6f310f95435a38e58
Aymen Chetoui <aymen.chetoui@gmail.com>
2015-08-29 22:05:33 +0100

    ELK Stack with OpsWorks

--- recipes/install-kibana.rb
+++ recipes/install-kibana.rb
@@ -25,15 +25,31 @@ end
 
 # Create a Kibana directory
 
-#execute "Add Kibana directory" do
-#	command "mkdir -p /opt/kibana"
-#end
+execute "Add Kibana directory" do
+	command "mkdir -p /opt/kibana"
+end
 
 # Copying the Kibana files into Kibana directory:
 
-#execute "Copy Kibana files" do
-#	command "sudo cp -R ~/kibana-4*/* /opt/kibana/"
-#end
+execute "Copy Kibana files" do
+	command "cp -R /kibana-4.0.1-linux-x64/* /opt/kibana/"
+end
+
+# Make Kibana start as a service
+
+execute "Download initi file for Kibana" do
+	command "wget --output-document=\"/etc/init.d/kibana\" https://raw.githubusercontent.com/cjcotton/init-kibana/master/kibana"
+end
+
+cookbook_file "/etc/init.d/kibana" do
+	source "kibana"
+
+end
+
+execute "Change init file mod" do
+	command "chmod +x /etc/init.d/kibana"
+end
+
 
 # Create systemd for Kibana
 
@@ -42,9 +58,61 @@ end
 #	mode "0644"
 #end
 
+# Add the EPEL repository to yum
+
+execute "Add the EPEL repository to yum" do
+	command "yum -y install epel-release"
+end
+
+# Install Nginx and httpd-tools:
+
+execute "Install Nginx and httpd-tools" do
+	command "yum -y install nginx httpd-tools"
+end
+
+# Modify Kibana yaml config file
+
+cookbook_file "/etc/nginx/nginx.conf" do
+	source "nginx.conf"
+	mode "0644"
+end
+
+# Add Kibana conf file
+
+template "/etc/nginx/conf.d/kibana.conf" do
+	source "kibana.conf.erb"
+	mode "0644"
+end
+
+
 # Start Kibana
 
-execute "Start Kibana" do
-	command "nohup ./kibana-4.0.1-linux-x64/bin/kibana &"
+#execute "Start Kibana" do
+#	command "./kibana-4.0.1-linux-x64/bin/kibana &"
+#end
+
+# Start Kibana
+
+#execute "Start Kibana" do
+#	command "/etc/init.d/kibana start"
+#end
+
+service "kibana" do
+	action :start
 end
+
+# Restart Ngix
+
+service "nginx" do
+	action :stop
+end
+
+service "nginx" do
+	action :start
+end
+
+service "kibana" do
+	action :restart
+end
+
 
